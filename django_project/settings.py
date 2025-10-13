88e271feff4c31175633f1cde2cc69804545da57
Kofi Brandful <rhutaford@gmail.com>
2025-10-13 03:36:20 +0000

    added required hosting

--- django_project/settings.py
+++ django_project/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = env.str("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool("DEBUG", default=False)
 
-ALLOWED_HOSTS = ["https://newspaper-3pd3.onrender.com", ".render.com", "localhost", "127.0.0.1"]
+ALLOWED_HOSTS = ["https://newspaper-3pd3.onrender.com","newspaper-3pd3.onrender.com", ".onrender.com", "localhost", "127.0.0.1"]
 
 
 # Application definition
